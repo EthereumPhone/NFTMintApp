eb14b6db376d6817e99112f23671076314c6aae9
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-05-14 18:04:23 +0200

    added logos/text, added Compose to project, added first compose elements. Finished splash screen.

--- app/src/main/java/org/ethereumphone/nftcreator/ui/components/WalletConnectButton.kt
+++ app/src/main/java/org/ethereumphone/nftcreator/ui/components/WalletConnectButton.kt
@@ -1,11 +1,19 @@
 package org.ethereumphone.nftcreator.ui.components
 
+import android.content.res.Resources
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.android.material.shape.ShapeAppearanceModel
 import dev.pinkroom.walletconnectkit.WalletConnectButton
 
 
@@ -18,7 +26,12 @@ fun WalletConnectButton(
     MaterialTheme {
         Button(
             onClick = onClick,
-            contentPadding = PaddingValues()
+            contentPadding = PaddingValues(
+            ),
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+
 
         ) {
             Text(text)
@@ -31,7 +44,7 @@ fun WalletConnectButton(
 @Composable
 @Preview(showBackground = false)
 fun WalletConnectButtonPreview() {
-    WalletConnectButton(text = "test", onClick = {})
+    WalletConnectButton(text = "connect wallet", onClick = {})
 }
 
 
--- app/src/main/java/org/ethereumphone/nftcreator/ui/screens/SplashScreen.kt
+++ app/src/main/java/org/ethereumphone/nftcreator/ui/screens/SplashScreen.kt
@@ -1,18 +1,69 @@
 package org.ethereumphone.nftcreator.ui.screens
 
 import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.res.ResourcesCompat.getColor
+import org.ethereumphone.nftcreator.R
 
 @ExperimentalComposeUiApi
 @Composable
 fun SplashScreen() {
+     Scaffold(
+        backgroundColor = colorResource(id = R.color.gray_dark),
+         bottomBar = { BottomAppBar(backgroundColor = colorResource(id = R.color.gray_dark))
+            {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_ethos),
+                    contentDescription = null,
+                    tint = Color.Unspecified,
+                    modifier = Modifier
+                        .fillMaxWidth())
+            }
+         }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier
+                    .fillMaxWidth(0.5F)
+            )
 
+            Icon(
+                painter = painterResource(id = R.drawable.ic_ters),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier
+                    .fillMaxHeight(0.1f)
+
+            )
+
+
+        }
+    }
 }
-
-
-
 
 
 
@@ -21,5 +72,5 @@ fun SplashScreen() {
 @ExperimentalComposeUiApi
 @Preview(showBackground = true)
 fun SplashScreenPreview() {
-
+    SplashScreen()
 }
