f4027638cb630cac828d24be520048d5d87a45df
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-06-01 21:34:08 +0200

    finished WalletConnectButton.kt. layed first UI elements in Home.kt

--- app/src/main/java/org/ethereumphone/nftcreator/ui/components/WalletConnectButton.kt
+++ app/src/main/java/org/ethereumphone/nftcreator/ui/components/WalletConnectButton.kt
@@ -1,28 +1,53 @@
 package org.ethereumphone.nftcreator.ui.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
+import androidx.lifecycle.ViewModel
+import dev.pinkroom.walletconnectkit.R
+import dev.pinkroom.walletconnectkit.WalletConnectKit
+import dev.pinkroom.walletconnectkit.WalletConnectKitConfig
+import org.walletconnect.Session
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun WalletConnectButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    imageModifier: Modifier = Modifier
+        .size(232.dp, 48.dp)
+        .padding(12.dp),
+    walletConnectKit: WalletConnectKit,
+    onConnected: (address: String) -> Unit,
+    onDisconnected: (() -> Unit)? = null,
+    sessionCallback: Session.Callback? = null
 ) {
+    val viewModel = WCKViewModel(walletConnectKit, onConnected, onDisconnected, sessionCallback)
+    viewModel.loadSessionIfStored()
     MaterialTheme {
-        Button(
-            onClick = onClick,
+        Button( // TODO fix background colors (no dark mode?)
+            //colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.design_default_color_background)),
+            onClick = viewModel::onClick,
             contentPadding = PaddingValues(),
-            shape = RoundedCornerShape(20.dp),
             modifier = modifier
+                .fillMaxWidth(0.8f),
+            shape = RoundedCornerShape(20.dp),
+            //border = BorderStroke(1.5.dp, colorResource(id = R.color.walletconnect_icon))
         ) {
             Text("Connect Wallet")
         }
@@ -30,11 +55,71 @@ fun WalletConnectButton(
 }
 
 
+internal class WCKViewModel constructor(
+    private val walletConnectKit: WalletConnectKit,
+    private val onConnected: (address: String) -> Unit,
+    private val onDisconnected: (() -> Unit)?,
+    private val sessionCallback: Session.Callback?
+) : ViewModel(), Session.Callback {
 
+    override fun onStatus(status: Session.Status) {
+        viewModelScope.launch {
+            when (status) {
+                is Session.Status.Approved -> onSessionApproved()
+                is Session.Status.Connected -> onSessionConnected()
+                is Session.Status.Closed -> onSessionDisconnected()
+            }
+            sessionCallback?.onStatus(status)
+        }
+    }
+
+    override fun onMethodCall(call: Session.MethodCall) {
+        viewModelScope.launch { sessionCallback?.onMethodCall(call) }
+    }
+
+    fun onClick() {
+        if (walletConnectKit.isSessionStored) {
+            walletConnectKit.removeSession()
+        }
+        walletConnectKit.createSession(this)
+    }
+
+    fun loadSessionIfStored() {
+        if (walletConnectKit.isSessionStored) {
+            walletConnectKit.loadSession(this)
+            walletConnectKit.address?.let(onConnected)
+        }
+    }
+
+    private fun onSessionApproved() {
+        walletConnectKit.address?.let(onConnected)
+    }
+
+    private fun onSessionConnected() {
+        walletConnectKit.address ?: walletConnectKit.requestHandshake()
+    }
+
+    private fun onSessionDisconnected() {
+        onDisconnected?.invoke()
+    }
+}
+
+@Preview
 @Composable
-@Preview(showBackground = false)
-fun WalletConnectButtonPreview() {
-    WalletConnectButton(modifier = Modifier.fillMaxWidth(0.9f), onClick = {})
+private fun ComposablePreview() {
+    val config = WalletConnectKitConfig(
+        context = LocalContext.current,
+        bridgeUrl = "wss://bridge.aktionariat.com:8887",
+        appUrl = "walletconnectkit.com",
+        appName = "WalletConnect Kit",
+        appDescription = "This is the Swiss Army toolkit for WalletConnect!"
+    )
+    val walletConnectKit = WalletConnectKit.Builder(config).build()
+    WalletConnectButton(
+        modifier = Modifier.fillMaxWidth(),
+        walletConnectKit = walletConnectKit,
+        onConnected = {},
+    )
 }
 
 
--- app/src/main/java/org/ethereumphone/nftcreator/ui/screens/Home.kt
+++ app/src/main/java/org/ethereumphone/nftcreator/ui/screens/Home.kt
@@ -1,23 +1,39 @@
 package org.ethereumphone.nftcreator.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.ethereumphone.nftcreator.R
 
 @Composable
 fun Home() {
     Scaffold(
-        backgroundColor = colorResource(id = R.color.gray_dark)
+        backgroundColor = colorResource(id = R.color.gray_dark),
+        bottomBar = { BottomAppBar(backgroundColor = colorResource(id = R.color.gray_dark))
+            {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_ethos),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier
+                    .fillMaxWidth())
+            }
+        }
     ) {
-        Column() {
+        Column(
+        ) {
             Column(
                 Modifier
                     .fillMaxWidth(),
@@ -28,12 +44,44 @@ fun Home() {
                     contentDescription = null,
                     tint = Color.Unspecified,
                     modifier = Modifier
-
+                        .padding(top = 20.dp),
                 )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                BoxWithConstraints(
+
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.85f)
+                        .padding(PaddingValues(top = 30.dp, start = 30.dp, end = 30.dp, bottom = 10.dp))
+                        .border(
+                            BorderStroke(5.dp, colorResource(id = R.color.purple_200)),
+                            shape = RectangleShape
+                        ),
+                ) {
+                }
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+
+                ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth(0.7f),
+                    shape = RoundedCornerShape(50)
+
+                ) {
+                    Text("Mint NFT")
+                }
             }
         }
     }
 }
+
 
 
 
