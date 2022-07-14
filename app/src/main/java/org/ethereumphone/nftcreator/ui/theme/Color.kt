0caa7a3de2768dbcef6629eed2e7a4f9fb4848b4
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-07-15 01:16:21 +0200

    changed grey shading

--- app/src/main/java/org/ethereumphone/nftcreator/ui/theme/Color.kt
+++ app/src/main/java/org/ethereumphone/nftcreator/ui/theme/Color.kt
@@ -6,4 +6,4 @@ import androidx.compose.ui.graphics.Color
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
-val black900 = Color(0xFF262626)
+val black900 = Color(0xFF2C2C2C)
