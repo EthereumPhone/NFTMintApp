e430aa2418eb642ccc79d7728b67b26dfbadff01
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-07-19 17:26:16 +0200

    fix UI position

--- app/src/main/java/org/ethereumphone/nftcreator/ui/screens/Login.kt
+++ app/src/main/java/org/ethereumphone/nftcreator/ui/screens/Login.kt
@@ -3,6 +3,8 @@ package org.ethereumphone.nftcreator.ui.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -10,9 +12,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import org.ethereumphone.nftcreator.R
+import org.ethereumphone.nftcreator.ui.components.OverlappingTriangles
 import org.ethereumphone.nftcreator.ui.components.TersLogo
 import org.ethereumphone.nftcreator.ui.screens.destinations.HomeDestination
 import org.ethereumphone.nftcreator.walletconnect.ConnectWalletViewModel
@@ -26,33 +30,13 @@ fun Login(
     walletConnect: ConnectWalletViewModel = get() // injected
 ) {
     val context = LocalContext.current
+    val walletAddress = walletConnect.userWallet.collectAsState().value
 
-    Scaffold(
-        //backgroundColor = MaterialTheme.colors.background,
-        bottomBar = { BottomAppBar {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_ethos),
-                    contentDescription = null,
-                    tint = Color.Unspecified,
-                    modifier = Modifier
-                        .fillMaxWidth())
-            }
-        }
-    ) {
-        // main
-        Column(
-            Modifier.fillMaxHeight(),
-        ) {
 
+    Scaffold {
+        Column{
             TersLogo()
-
-            Icon(
-                painter = painterResource(id = R.drawable.ic_arrows),
-                contentDescription = "ters logo",
-                tint = Color.Unspecified,
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
+            OverlappingTriangles()
 
             Column(
                 Modifier
