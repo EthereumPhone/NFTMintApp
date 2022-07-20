6acd353005351f498df9e6db49113fa05ffdb16b
Markus Haas <haas.markus@protonmail.com>
2022-07-20 17:20:51 +0200

    Added completablefutures draft

--- app/src/main/java/org/ethereumphone/nftcreator/utils/NftMinting.kt
+++ app/src/main/java/org/ethereumphone/nftcreator/utils/NftMinting.kt
@@ -2,15 +2,32 @@ package org.ethereumphone.nftcreator.utils
 
 import com.immutable.sdk.Signer
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
 
 
 class ImxSigner : Signer {
     override fun getAddress(): CompletableFuture<String> {
-        TODO("Not yet implemented")
+        val completableFuture = CompletableFuture<String>()
+
+        Executors.newCachedThreadPool().submit {
+            Thread.sleep(500)
+            //TODO: Return valid connected address
+            completableFuture.complete("Hello")
+        }
+
+        return completableFuture
     }
 
     override fun signMessage(message: String): CompletableFuture<String> {
-        TODO("Not yet implemented")
+        val completableFuture = CompletableFuture<String>()
+
+        Executors.newCachedThreadPool().submit {
+            Thread.sleep(500)
+            //TODO: Sign passed message and return result
+            completableFuture.complete("Hello")
+        }
+
+        return completableFuture
     }
 
 }
