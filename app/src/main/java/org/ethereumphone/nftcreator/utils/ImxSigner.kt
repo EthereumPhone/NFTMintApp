2b59f9dfb3908aef8d0e4d7a6cb8108da17e5863
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-07-21 21:45:51 +0200

    modify StarkSigner

--- app/src/main/java/org/ethereumphone/nftcreator/utils/ImxSigner.kt
+++ app/src/main/java/org/ethereumphone/nftcreator/utils/ImxSigner.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import androidx.compose.ui.platform.LocalContext
 import com.immutable.sdk.Signer
 import com.immutable.sdk.StarkSigner
+import com.immutable.sdk.crypto.StarkKey
 import dev.pinkroom.walletconnectkit.WalletConnectKit
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.ethereumphone.nftcreator.walletconnect.ConnectWalletViewModel
+import org.web3j.crypto.ECKeyPair
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 
@@ -17,8 +19,8 @@ class ImxSigner(
     walletViewModel: ConnectWalletViewModel? = null,
     walletConnectKit: WalletConnectKit,
     context: Context
-) : Signer, StarkSigner {
-    private val wallet = walletViewModel
+) : Signer {
+    //private val wallet = walletViewModel
     private val context = context
     private val walletConnectKit = walletConnectKit
 
@@ -52,6 +54,40 @@ class ImxSigner(
             }
         }
 
+        return completableFuture
+    }
+}
+
+class ImxStarkSinger(
+    walletConnectKit: WalletConnectKit,
+    context: Context
+): StarkSigner {
+    private val walletConnectKit = walletConnectKit
+    private val context = context
+
+    override fun getAddress(): CompletableFuture<String> {
+        val completableFuture = CompletableFuture<String>()
+
+        Executors.newCachedThreadPool().submit {
+            //completableFuture.complete(wallet?.userWallet?.value)
+            completableFuture.complete(walletConnectKit.address)
+
+        }
+        return completableFuture
+    }
+
+    override fun signMessage(message: String): CompletableFuture<String> {
+        val completableFuture = CompletableFuture<String>()
+
+        Executors.newCachedThreadPool().submit {
+            StarkKey.generate(ImxSigner(walletConnectKit = walletConnectKit, context = context)).whenComplete { keyPair, error ->
+                val signOutput = StarkKey.sign(keyPair, message)
+
+                completableFuture.complete(signOutput)
+            }
+
+
+        }
         return completableFuture
     }
 
