2b438d4156e6fb9e154e6e57fc945301aa3ddd4e
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-07-21 19:25:26 +0200

    added WalletConnectKit

--- app/src/main/java/org/ethereumphone/nftcreator/utils/ImxSigner.kt
+++ app/src/main/java/org/ethereumphone/nftcreator/utils/ImxSigner.kt
@@ -1,21 +1,34 @@
 package org.ethereumphone.nftcreator.utils
 
+import android.content.Context
+import androidx.compose.ui.platform.LocalContext
 import com.immutable.sdk.Signer
 import com.immutable.sdk.StarkSigner
+import dev.pinkroom.walletconnectkit.WalletConnectKit
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.ethereumphone.nftcreator.walletconnect.ConnectWalletViewModel
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 
 
 class ImxSigner(
-    walletViewModel: ConnectWalletViewModel
+    walletViewModel: ConnectWalletViewModel? = null,
+    walletConnectKit: WalletConnectKit,
+    context: Context
 ) : Signer, StarkSigner {
     private val wallet = walletViewModel
+    private val context = context
+    private val walletConnectKit = walletConnectKit
+
     override fun getAddress(): CompletableFuture<String> {
         val completableFuture = CompletableFuture<String>()
 
         Executors.newCachedThreadPool().submit {
-            completableFuture.complete(wallet.userWallet.value)
+            //completableFuture.complete(wallet?.userWallet?.value)
+            completableFuture.complete(walletConnectKit.address)
+
         }
 
         return completableFuture
@@ -23,13 +36,22 @@ class ImxSigner(
 
     override fun signMessage(message: String): CompletableFuture<String> {
         val completableFuture = CompletableFuture<String>()
-        wallet.signMessage(
-            message = message
+        /*
+        wallet?.signMessage(
+            message = message,
+            context = context
         ) {
             Executors.newCachedThreadPool().submit {
                 completableFuture.complete(it.result.toString())
             }
         }
+        */
+        Executors.newCachedThreadPool().submit {
+            GlobalScope.launch(Dispatchers.Main) {
+                completableFuture.complete(walletConnectKit.personalSign(message).result.toString())
+            }
+        }
+
         return completableFuture
     }
 
