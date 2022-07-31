4aed73a4a09bd2f695e5d6cb3b90cfb754646f1c
Markus Haas <haas.markus@protonmail.com>
2022-07-31 17:12:21 +0200

    Change getAuth to mintTokens

--- app/src/main/java/org/ethereumphone/nftcreator/utils/mintToken.kt
+++ app/src/main/java/org/ethereumphone/nftcreator/utils/mintToken.kt
@@ -10,9 +10,9 @@ import java.net.URL
 import java.net.URLEncoder
 import java.util.concurrent.CompletableFuture
 
-fun getSignature(mint: MintRequest, address: String, mintData: MintTokenDataV2): CompletableFuture<String> {
+fun mintToken(address: String, mintData: MintTokenDataV2, ipfsHash: String): CompletableFuture<String> {
     val completableFuture = CompletableFuture<String>();
-    val url = "https://us-central1-imx-minting-ethos.cloudfunctions.net/getSignature?address=${URLEncoder.encode(address, "utf-8")}&id=${URLEncoder.encode(mintData.id, "utf-8")}&blueprint=${URLEncoder.encode(mintData.blueprint, "utf-8")}&contractAddress=${URLEncoder.encode(mint.contractAddress, "utf-8")}"
+    val url = "https://us-central1-imx-minting-ethos.cloudfunctions.net/mint?wallet=${URLEncoder.encode(address, "utf-8")}&tokenID=${URLEncoder.encode(mintData.id, "utf-8")}&ipfsHash=${URLEncoder.encode(ipfsHash, "utf-8")}"
     GlobalScope.launch(Dispatchers.IO) {
         val response = async { URL(url).readText() }.await()
         completableFuture.complete(response)
