26bf6d4394deb6e6c78236bd94141093f06096bc
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-11-13 23:05:03 +0100

    Created minting screen view

---
+++ app/src/main/java/org/ethereumphone/nftcreator/ui/screens/MintingScreen.kt
@@ -0,0 +1,93 @@
+package org.ethereumphone.nftcreator.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ramcosta.composedestinations.annotation.Destination
+import org.ethereumphone.nftcreator.ui.components.ImageBox
+import org.ethereumphone.nftcreator.ui.components.InputField
+import org.ethereumphone.nftcreator.ui.theme.NftCreatorTheme
+
+// https://www.youtube.com/watch?v=8waTylS0wUc
+
+@ExperimentalComposeUiApi
+@Composable
+@Destination(start = true)
+fun MintingScreen(
+){
+    Scaffold{
+        Column(
+            modifier = Modifier
+                .statusBarsPadding()
+                .navigationBarsPadding()
+                .imePadding()
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+        ) {
+            MintingScreenInput()
+        }
+    }
+}
+
+@Composable
+fun MintingScreenInput(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(25.dp),
+        modifier = modifier
+            .height(LocalConfiguration.current.screenHeightDp.dp)
+            .padding(40.dp)
+
+    ) {
+        Text(
+            text = "Mint new NFT",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+        ImageBox(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.35f)
+        )
+
+        InputField(
+            label = "Title",
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        InputField(
+            label = "Description",
+            singeLine = false,
+            maxLines = 5,
+            shape = RoundedCornerShape(25.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(.4f)
+        )
+    }
+}
+
+
+@ExperimentalComposeUiApi
+@Composable
+@Preview
+fun PreviewMintingScreen() {
+    NftCreatorTheme {
+        MintingScreen()
+    }
+}
