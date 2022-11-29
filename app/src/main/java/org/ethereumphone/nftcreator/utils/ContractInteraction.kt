31c52a68101ef02a2c4aea4892d8d59ca74a3bd7
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-11-29 23:13:38 +0100

    overloaded getEstimation

--- app/src/main/java/org/ethereumphone/nftcreator/utils/ContractInteraction.kt
+++ app/src/main/java/org/ethereumphone/nftcreator/utils/ContractInteraction.kt
@@ -10,19 +10,21 @@ import org.web3j.protocol.Web3j
 import org.web3j.protocol.http.HttpService
 import org.web3j.crypto.Credentials
 import org.web3j.protocol.core.methods.request.Transaction
+import org.web3j.protocol.core.methods.response.EthEstimateGas
 import org.web3j.tx.gas.DefaultGasProvider
 import java.math.BigInteger
 
 private const val GOERLI_CONTRACT_ADDRESS = "0x5B48267F7fDb98416C8382C230f4f4AD7453aBd7"
+
 private var web3j: Web3j? = null
 private var nftMintContract: Abi? = null
 private var selectedContract: String = ""
-val credentials = Credentials.create(Keys.createEcKeyPair())
+private var gasEstimate: EthEstimateGas? = null
+val credentials: Credentials = Credentials.create(Keys.createEcKeyPair())
 
 fun load(
     contract: String,
 ) {
-
     selectedContract = contract
     web3j = try {
         Web3j.build(HttpService())
@@ -39,13 +41,12 @@ fun load(
 
 }
 
-@Composable
-fun mintImage(
-    address: String?,
-    tokenURI: String?
+
+fun gasEstimation(
+    address: String,
+    tokenURI: String
 ) {
     val data = nftMintContract?.mintImage(address, tokenURI)?.encodeFunctionCall()
-    val wallet = WalletSDK(LocalContext.current)
 
     val transaction = Transaction.createFunctionCallTransaction(
         credentials.address,
@@ -55,15 +56,41 @@ fun mintImage(
         selectedContract,
         BigInteger.valueOf(10000000000), data)
 
-    val gasEstimation = web3j?.ethEstimateGas(transaction)?.sendAsync()?.get()
+    gasEstimate = web3j?.ethEstimateGas(transaction)?.sendAsync()?.get()
+}
+
+fun gasEstimation(
+    data: String
+) {
+    val transaction = Transaction.createFunctionCallTransaction(
+        credentials.address,
+        BigInteger.ONE,
+        web3j?.ethGasPrice()?.sendAsync()?.get()?.gasPrice,
+        DefaultGasProvider.GAS_LIMIT,
+        selectedContract,
+        BigInteger.valueOf(10000000000), data)
+
+    gasEstimate = web3j?.ethEstimateGas(transaction)?.sendAsync()?.get()
+}
+
+@Composable
+fun mintImage(
+    address: String,
+    tokenURI: String
+) {
+    val data = nftMintContract?.mintImage(address, tokenURI)?.encodeFunctionCall()
+    val wallet = WalletSDK(LocalContext.current)
+    if (data != null) {
+        gasEstimation(data)
+    }
 
     if (data != null) {
-        if (gasEstimation != null) {
+        gasEstimate?.let {
             wallet.sendTransaction(
                 to = selectedContract,
                 value = "0x0",
                 data = data,
-                gasAmount = gasEstimation.result
+                gasAmount = it.result
             )
         }
     }
